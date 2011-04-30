e3816a9f5fe889e58c63605ab70145b181aac2e0
Nathan Sturgess <nathansturgess.uni@gmail.com>
2011-05-01 08:39:10 +1000

    Added getAlpha() and getAlphaNumeric() to Space class.

--- src/Space.java
+++ src/Space.java
@@ -28,4 +28,31 @@ public class Space {
 			return false;
 		}
 	}
+	
+	public String getAlpha() {
+		switch(this.getX()) {
+			case 1: return "a"; 
+			case 2: return "b";
+			case 3: return "c"; 
+			case 4: return "d"; 
+			case 5: return "e"; 
+			case 6: return "f"; 
+			case 7: return "g"; 
+			case 8: return "h"; 
+			case 9: return "i"; 
+			default: return "Error x coordinate is out of bounds";
+		}
+	}
+	
+	public String getAlphaNumeric() {
+		return (this.getAlpha()+this.getY());
+	}
+	
+	//test
+	public static void main(String[] args) {
+		Space space = new Space (0,1);
+		System.out.println(space.getAlphaNumeric());
+		space.setCoords(1, 5);
+		System.out.println(space.getAlphaNumeric());
+	}
 }
