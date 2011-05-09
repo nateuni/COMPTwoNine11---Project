1e29fdfee7b1f1e1a661e8db05c4d26c5afc1d04
Elyse Wise <elyse.wise@gmail.com>
2011-05-09 18:30:19 +1000

    lab 9 part 2

--- src/quoridor/Human_Human_Game.java
+++ src/quoridor/Human_Human_Game.java
@@ -43,27 +43,36 @@ public class Human_Human_Game extends AbstractGame{
 	//returns true if player has completed a valid turn.
     private Boolean handleTurn(String command) {
     	// if it is a wall move
-    	if(!checkInput(command)) {
+    	try {
+    	int inputType = checkInput(command);
+    	if(inputType <0) {
     		return false;
     	}
-		
-		return moveCurrentPlayer(command);
+    	if(inputType == 0) {
+    		checkValidMovement(command);
+    		Space newSpace = new Space(command);
+    		moveCurrentPlayer(space);
+    	}
+    	if(inputType == 1) {
+    		
+    		checkValidWall(command)
+    	}
+    	}
+    	catch( Exception e) {
+    		//all exceptions would be from trying to read incorrect formatting on user input
+    		return false;
+    	}
 	}
     
-    //returns 0 for a valid movement move, 1 for a valid wall move and -1 for an invalid request.
-    checkInput(String command) {
+    //returns 0 for a movement move, 1 for a wall move and -1 for an invalid request.
+    private int checkInput(String command) {
     	if(command.substring(0,1).equals("V") || command.substring(0,1).equals("H")){
-    		if(!checkValidWall(command)) {
-    			return -1;
-    		}
     		return 1;
 		}
     	else if(command.length() == 2) {
-    		if(!checkValidMove(command)) {
-    			return -1;
-    		}
     		return 0;
     	}
+    	return -1;
     }
 
 	private boolean checkValidWall(String command) {
