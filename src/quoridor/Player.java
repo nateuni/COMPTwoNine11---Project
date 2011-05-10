23720ad9191eaf7eecf080d3f2dbafc544dd6daf
Eddie McLean <middlerun@gmail.com>
2011-05-10 23:38:02 +1000

    Fixed indentation in Player. (I tried to fix it in the previous edit and it just ended up broken in a slightly different way. :S Should be good now.)

--- src/quoridor/Player.java
+++ src/quoridor/Player.java
@@ -10,22 +10,22 @@ public abstract class Player {
 	protected String token;
 	protected Space space;
 	int wallsLeft = 10;
-	
+
 	public Player(int playerNumber) {
 		if (playerNumber == 1) {
 			name = " X ";
-            this.space = new Space("e1");
-        }
-        else {
-            name = " O ";
-            this.space = new Space("e9");
-        }
-        setToken(name);
+			this.space = new Space("e1");
+		}
+		else {
+			name = " O ";
+			this.space = new Space("e9");
+		}
+		setToken(name);
 	}
 
-    protected void setToken(String t) {
-        this.token = t;
-    }
+	protected void setToken(String t) {
+		this.token = t;
+	}
 
 	/**
 	 * @return The player's token as a String
@@ -33,11 +33,11 @@ public abstract class Player {
 	public String getToken() {
 		return token;
 	}
-	
+
 	public Boolean hasWallsLeft() {
 		return (wallsLeft > 0); 
 	}
-	
+
 	public Boolean decrementWallTally() {
 		if(this.hasWallsLeft()){
 			this.wallsLeft--;
@@ -45,7 +45,7 @@ public abstract class Player {
 		}
 		return false;
 	}
-	
+
 	//used for undo'ing
 	public boolean incrementWallTally(){
 		if(this.wallsLeft < 20){
@@ -54,30 +54,29 @@ public abstract class Player {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * @return The player's name as a String 
 	 */
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
 	 * @return The players current Space as a pointer to a Space object. 
 	 */
 	public Space getSpace() {
 		return space;
 	}
-	
+
 	public void setSpace(Space newSpace) {
 		space = newSpace;
 	}
 
-    public abstract Move getMove();
+	public abstract Move getMove();
 
 	public String toString() {
 		return name;
 	}
-	
+
 }
-		
