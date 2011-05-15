d49891097098fce3fb24048fe9764cbb3267f342
Nathan Sturgess <nathansturgess.uni@gmail.com>
2011-05-15 20:53:41 +1000

    edited moveValid() to use the adjacentSpace() method. Checked moves/walls in Validator to see output.

--- src/quoridor/test/TestGameInterface.java
+++ src/quoridor/test/TestGameInterface.java
@@ -4,7 +4,11 @@ import quoridor.*;
 public class TestGameInterface {
 		
 		public static void main(String[] args) {
-			Validator.check("e2");
+			if(Validator.check("e2 e3h e3 e8 d3 e7 d6h d7 b6h c3h a7v")) {
+				System.out.print("Move sequence is all good");
+			} else {
+				System.out.print("Invalid Move sequence");
+			}
 		}
 	}
 
