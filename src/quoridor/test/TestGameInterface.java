bf7de39c16368ec6802109ebaf34004c6ad25799
nateuni <nathansturgess.uni@gmail.com>
2011-05-16 16:51:15 +1000

    Ignore - as I just tested Validator some more moves in TestGameInterface

--- src/quoridor/test/TestGameInterface.java
+++ src/quoridor/test/TestGameInterface.java
@@ -4,7 +4,7 @@ import quoridor.*;
 public class TestGameInterface {
 		
 		public static void main(String[] args) {
-			if(Validator.check("e2 e3h e3 e8 d3 e7 d6h d7 b6h c3h a7v")) {
+			if(Validator.check("e2 e3h e3 e8 d3 e7 d6h d7 b6h c3h a7v e7 e3 f7 f3 g7 g3 g6 g4 g5 f4 g4 f5 g3 f4 g2 f5 g1 f6")) {
 				System.out.print("Move sequence is all good");
 			} else {
 				System.out.print("Invalid Move sequence");
