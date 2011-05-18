6f58e29fcb0e947208b333f4c3d5fa4ac637d92d
Eddie McLean <middlerun@gmail.com>
2011-05-18 12:10:21 +1000

    Implemented cutsOffPath() for checking wall moves. (Not tested yet.) Added Factory.make(), because Factory.instance() was returning null due to theFactory not being initialised. Some moves are causing the game to exit, not sure why.

--- src/quoridor/ValidatorGame.java
+++ src/quoridor/ValidatorGame.java
@@ -9,6 +9,7 @@ public class ValidatorGame extends Game{
 	int gameType = 3;
 	
 	public ValidatorGame(String string) {
+		Factory.make();
 		if(string == null){
 			throw new RuntimeException("String cannot be null");
 		}
