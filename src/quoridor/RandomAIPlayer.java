36f6624283d9dd64d481656b5c384c77a27c5abc
Nathan Sturgess <nathansturgess.uni@gmail.com>
2011-05-21 12:07:43 +1000

    Still trying to commit..

---
+++ src/quoridor/RandomAIPlayer.java
@@ -0,0 +1,38 @@
+package quoridor;
+
+public class RandomAIPlayer extends AIPlayer {
+
+	public RandomAIPlayer(int playerNumber) {
+		super(playerNumber);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Move getMove(Board board) {
+		Move move = null;
+		boolean valid = false;
+		while (!valid) {
+			try {
+				if (Math.random() < 0.6) {
+					// Do a movement move
+					double dir = Math.random();
+					if      (dir < 0.25) move = new MovementMove(this.getSpace(), this.getSpace().getUp());
+					else if (dir < 0.5)  move = new MovementMove(this.getSpace(), this.getSpace().getRight());
+					else if (dir < 0.75) move = new MovementMove(this.getSpace(), this.getSpace().getDown());
+					else                 move = new MovementMove(this.getSpace(), this.getSpace().getLeft());
+				}
+				else {
+					// Do a wall move
+					int row = (int) Math.floor(Math.random() * 9);
+					int col = (int) Math.floor(Math.random() * 9);
+					boolean vertical = (Math.random() < 0.5);
+					move = new WallMove(new Wall(new Space(col, row), vertical));
+				}
+				valid = board.moveValid(move);
+			}
+			catch (RuntimeException e) {
+				valid = false;
+			}
+		}
+		return move;
+	}
+}
