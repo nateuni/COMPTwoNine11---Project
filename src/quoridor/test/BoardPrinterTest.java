0b222f5d0573e0c1a3a00ad78886fd07a5ce51a4
Eddie McLean <middlerun@gmail.com>
2011-05-29 22:06:13 +1000

    Cleaned up BoardGraph and a couple of the tests.

--- src/quoridor/test/BoardPrinterTest.java
+++ src/quoridor/test/BoardPrinterTest.java
@@ -5,24 +5,14 @@ import quoridor.*;
 public class BoardPrinterTest {
 
 	public static void main(String[] args) {
-		//Two<Player> players = Two.two(new Player("Alice", 5, 9), new Player("Bob", 5, 1));
-		Board board = Factory.instance().makeBoard(3);
+		Board board = Factory.instance().makeBoard(0);
 		board.addWall(new Wall (new Space (2,2), false));
 		board.addWall(new Wall (new Space (4,1), false));
-		/*
-		board.addWall(1, 0, false);
-		board.addWall(3, 0, false);
-		board.addWall(0, 3, true);
-		board.addWall(3, 7, false);
-		board.addWall(5, 7, false);
-		board.addWall(4, 7, true);
-		board.addWall(7, 4, true);
-		board.addWall(7, 6, true);
-		board.addWall(7, 5, false);
-		*/
 		BoardPrinter.setStyle(0);
 		board.print();
 		BoardPrinter.setStyle(1);
+		board.print();
+		BoardPrinter.setStyle(2);
 		board.print();
 	}
 }
