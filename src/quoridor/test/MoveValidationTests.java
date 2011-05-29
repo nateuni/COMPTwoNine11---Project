d5ab09500ca6999cd90fce1c8a1eeb7daa8abc48
Elyse Wise <elyse.wise@gmail.com>
2011-05-29 11:35:49 +1000

    All test now working.

--- src/quoridor/test/MoveValidationTests.java
+++ src/quoridor/test/MoveValidationTests.java
@@ -45,38 +45,46 @@ public class MoveValidationTests {
 		assertFalse(game.playGame());
 	}
 	
+	
+	//fixed from here
 	@Test
 	public void NoWinner() {
-		String testString = "e2 e8 e3 e7 e4 d7 e5";
+		String testString = "e8 e2 e7 e3 d7 e4";
 		Game game = Factory.instance().makeGame(testString);
 		assertTrue(game.playGame() && game.checkWin() == 0);
 	}
 	
+	//
 	@Test
 	public void testP1Winner() {
-		String testString = "e2 e8 e3 e7 e4 d7 e5 d6 e6 d5 e7 d4 e8 d3 e9";
+		String testString = "e8 d1 e7 d2 e6 d3 e5 d4 e4 d5 e3 d6 e2 d7 e1";
 		Game game = Factory.instance().makeGame(testString);
 		assertTrue(game.playGame() && game.checkWin() == 1);
 	}
 	
+	
+	//
 	@Test
 	public void testP1WinnerThusP2MoveInvalid() {
-		String testString = "e2 e8 e3 e7 e4 d7 e5 d6 e6 d5 e7 d4 e8 d3 e9 d2";
+		String testString = "e8 d1 e7 d2 e6 d3 e5 d4 e4 d5 e3 d6 e2 d7 e1 d8";
 		Game game = Factory.instance().makeGame(testString);
 		assertFalse(game.playGame());
 		assertTrue(game.checkWin() == 1);
 	}
 	
+	//
 	@Test
 	public void testP2Winner() {
-		String testString = "d1 e8 d2 e7 d3 e6 d4 e5 d5 e4 d6 e3 d7 e2 d8 e1";
+
+		String testString = "e8 e2 e7 e3 d7 e4 d6 e5 d5 e6 d4 e7 d3 e8 d2 e9";
 		Game game = Factory.instance().makeGame(testString);
 		assertTrue(game.playGame() && game.checkWin() == 2);
 	}
 	
+	//
 	@Test
 	public void testP2WinnerThusP1MoveInvalid() {
-		String testString = "d1 e8 d2 e7 d3 e6 d4 e5 d5 e4 d6 e3 d7 e2 d8 e1 d9";
+		String testString = "e8 e2 e7 e3 d7 e4 d6 e5 d5 e6 d4 e7 d3 e8 d2 e9 d3";	
 		Game game = Factory.instance().makeGame(testString);
 		assertFalse(game.playGame());
 		assertTrue(game.checkWin() == 2);
@@ -90,6 +98,7 @@ public class MoveValidationTests {
 		assertTrue(game.checkWin() == 0);
 	}
 	
+	//
 	@Test
 	public void testPlaceTwentyWalls(){
 		String testString = "a2h c2h e2h g2h a3h c3h e3h g3h a4h c4h e4h g4h a5h c5h e5h g5h a6h c6h e6h g6h";
@@ -98,7 +107,7 @@ public class MoveValidationTests {
 		assertTrue(game.checkWin() == 0);
 	}
 	
-	
+	//
 	@Test
 	public void testInvalidToPlaceTwentyPlusWalls(){
 		String testString = "a2h c2h e2h g2h a3h c3h e3h g3h a4h c4h e4h g4h a5h c5h e5h g5h a6h c6h e6h g6h a7h";
@@ -107,9 +116,10 @@ public class MoveValidationTests {
 		assertTrue(game.checkWin() == 0);
 	}
 	
+	
 	@Test
 	public void testInvalidForPlayerToPlaceMoreThenTenWallsPerPlayer(){
-		String testString = "f1 a1h g1 a2h h1 a3h i1 a4h i2 a5h i3 a6h i4 a7h i5 a8h i6 c1h i7 c2h i8 c5h";
+		String testString = "d9 f1 a1h g1 a2h h1 a3h i1 a4h i2 a5h i3 a6h i4 a7h i5 a8h i6 c1h i7 c2h i8 c5h";
 		Game game = Factory.instance().makeGame(testString);
 		assertFalse(game.playGame());
 		assertTrue(game.checkWin() == 0);
@@ -117,7 +127,7 @@ public class MoveValidationTests {
 	
 	@Test
 	public void testStayingInOriginalSpotDoesNotCountAsAWin(){
-		String testString = "f1 a1h g1 a2h h1 a3h i1 a4h i2 a5h i3 a6h i4 a7h i5 a8h i6 c1h i7 c2h i8";
+		String testString = "d9 f1 a1h g1 a2h h1 a3h i1 a4h i2 a5h i3 a6h i4 a7h i5 a8h i6 c1h i7 c2h i8";
 		Game game = Factory.instance().makeGame(testString);
 		assertTrue(game.playGame());
 		assertTrue(game.checkWin() == 0);
@@ -125,7 +135,7 @@ public class MoveValidationTests {
 	
 	@Test
 	public void saveLoadGame(){
-		String testString = "f1 a1h g1 a2h h1 a3h i1 a4h i2 a5h i3";
+		String testString = "d9 f1 a1h g1 a2h h1 a3h i1 a4h i2 a5h i3";
 		Game game = Factory.instance().makeGame(testString);
 		assertTrue(game.playGame());
 		assertTrue(game.save());
